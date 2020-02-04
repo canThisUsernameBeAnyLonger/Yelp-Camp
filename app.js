df011505b0238b9e823c17c00a90e63d8c7db9d6
Deepak <deepak.bibyan51098@gmail.com>
2020-02-04 20:25:48 +0530

    add user authentication

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ var mongoose=require("mongoose");
 var Campground=require("./models/campground");
 var seedDB=require("./seeds");
 var Comment=require("./models/comment");
+var passport=require("passport");
+var LocalStrategy=require("passport-local");
+var User=require("./models/user");
 
 seedDB();
 
@@ -15,6 +18,22 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
+app.use(require("express-session")({
+	secret: "Secret passage idk",
+	resave: false,
+	saveUninitialized:false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req,res,next){
+	res.locals.currentUser=req.user;
+	next();
+});
+
 app.get("/", function(req,res){
 	res.render("landing");
 })
@@ -24,7 +43,7 @@ app.get("/campgrounds", function(req,res){
 		if(err){
 			console.log(err);
 		}else{
-			res.render("campgrounds/index",{campgrounds:allCampgrounds});
+			res.render("campgrounds/index",{campgrounds:allCampgrounds, currentUser:req.user});
 		}
 	})
 })
@@ -58,7 +77,7 @@ app.get("/campgrounds/:id",function(req,res){
 	})
 });
 
-app.get("/campgrounds/:id/comments/new", function(req,res){
+app.get("/campgrounds/:id/comments/new", isLoggedIn, function(req,res){
 	Campground.findById(req.params.id, function(err, campground){
 		if (err) {
 			console.log(err);
@@ -68,7 +87,7 @@ app.get("/campgrounds/:id/comments/new", function(req,res){
 	});
 });
 
-app.post("/campgrounds/:id/comments", function(req,res){
+app.post("/campgrounds/:id/comments", isLoggedIn, function(req,res){
 	Campground.findById(req.params.id, function(err,campground){
 		if (err) {
 			console.log(err);
@@ -86,6 +105,46 @@ app.post("/campgrounds/:id/comments", function(req,res){
 		}
 	});
 });
+
+app.get("/register", function(req, res){
+	res.render("register");
+});
+
+app.post("/register", function(req,res){
+	var newUser=new User({username: req.body.username});
+	User.register(newUser, req.body.password, function(err,user){
+		if (err) {
+			console.log(err);
+			return res.render("/register");
+		}
+		passport.authenticate("local")(req,res,function(){
+			res.redirect("/campgrounds");
+		});
+	});
+});
+
+app.get("/login", function(req,res){
+	res.render("login");
+});
+
+app.post("/login", passport.authenticate("local",
+	{
+		successRedirect: "/campgrounds",
+		failureRedirect: "/login"
+	}), function(req,res){
+});
+
+app.get("/logout", function(req,res){
+	req.logout();
+	res.redirect("/campgrounds");
+});
+
+function isLoggedIn(req,res,next) {
+	if (req.isAuthenticated()) {
+		return next();
+	}
+	res.redirect("/login");
+};
 
 app.listen(3000,function(){
 	console.log("YelpCamp has started");
