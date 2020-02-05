229c8068f13d31e698511705f8b3cf2b6b8f36cc
Deepak <deepak.bibyan51098@gmail.com>
2020-02-05 21:08:53 +0530

    add campground edit and update

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var Comment=require("./models/comment");
 var passport=require("passport");
 var LocalStrategy=require("passport-local");
 var User=require("./models/user");
+var mehtodOverride=require("method-override");
 
 var commentRoutes=require("./routes/comments");
 var campgroundRoutes=require("./routes/campgrounds");
@@ -21,6 +22,7 @@ mongoose.connect("mongodb://localhost:27017/yelp_camp",
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
+app.use(mehtodOverride("_method"));
 
 app.use(require("express-session")({
 	secret: "Secret passage idk",
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -45,6 +45,27 @@ router.get("/:id",function(req,res){
 	})
 });
 
+router.get("/:id/edit", function(req, res){
+	Campground.findById(req.params.id, req.body.campground,function(err, foundCampground){
+		if (err) {
+			console.log(err);
+		} else {
+			res.render("campgrounds/edit", {campground: foundCampground});
+
+		}
+	})
+});
+
+router.put("/:id", function(req, res){
+	Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, updatedCampground){
+		if (err) {
+			console.log(err);
+		} else {
+			res.render("/campgrounds/" + req.params.id);
+		}
+	});
+});
+
 function isLoggedIn(req,res,next) {
 	if (req.isAuthenticated()) {
 		return next();
