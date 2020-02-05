86160e29471dfebdc7688753201f814a8b26fcf5
Deepak <deepak.bibyan51098@gmail.com>
2020-02-05 22:35:01 +0530

    add campground CRUD

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -45,23 +45,28 @@ router.get("/:id",function(req,res){
 	})
 });
 
-router.get("/:id/edit", function(req, res){
-	Campground.findById(req.params.id, req.body.campground,function(err, foundCampground){
-		if (err) {
-			console.log(err);
-		} else {
-			res.render("campgrounds/edit", {campground: foundCampground});
-
-		}
-	})
+router.get("/:id/edit", checkCampgroundOwnership, function(req, res){
+	Campground.findById(req.params.id, function(err, foundCampground){
+		res.render("campgrounds/edit", {campground: foundCampground});
+	});
 });
 
 router.put("/:id", function(req, res){
 	Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, updatedCampground){
 		if (err) {
-			console.log(err);
+			res.redirect("/campgrounds");
 		} else {
 			res.render("/campgrounds/" + req.params.id);
+		}
+	});
+});
+
+router.delete("/:id", checkCampgroundOwnership, function(req,res){
+	Campground.findByIdAndRemove(req.params.id, function(err){
+		if (err) {
+			res.redirect("/campgrounds");
+		} else {
+			res.redirect("/campgrounds");
 		}
 	});
 });
@@ -71,6 +76,24 @@ function isLoggedIn(req,res,next) {
 		return next();
 	}
 	res.redirect("/login");
+};
+
+function checkCampgroundOwnership(req,res,next) {
+	if (req.isAuthenticated()) {
+		Campground.findById(req.params.id, req.body.campground,function(err, foundCampground){
+			if (err) {
+				res.redirect("back");
+			} else {
+				if (foundCampground.author.id.equals(req.user._id)) {
+					next();
+				} else {
+					res.send("You don't have authorization");
+				}
+			}
+		});
+	} else {
+		res.redirect("back");
+	};
 };
 
 module.exports=router;
